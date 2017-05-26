645ab22b0081a4c4983445c91519488430ece466
abezuska <abezuska@gmail.com>
2017-05-26 10:05:57 -0400

    improve extract speed, add moveFiles flag, improve config prop names

--- extract.js
+++ extract.js
@@ -7,52 +7,31 @@ if (!config.imageFileTypes && !config.videoFileTypes) {
   console.log("no 'imageFileTypes' or 'videoFileTypes' set in config.json... doing nothing.");
 }
 
-if (!config.imageDestination && !config.videoDestination) {
+if (!config.imageDestinationFolder && !config.videoDestinationFolder) {
   console.log("no 'imageDestination' or 'videoDestination' set in config.json... doing nothing.");
 }
 
-if (config.imageFileTypes && config.imageDestination) {
-  var images = extractFiles(config.imageFileTypes, config.source);
-  moveFiles(images, config.imageDestination);
+if (config.imageFileTypes && config.imageDestinationFolder) {
+  extractFiles(config.imageFileTypes, config.sourceFolder, config.imageDestinationFolder);
 }
 
-if (config.videoFileTypes && config.videoDestination) {
-  var videos = extractFiles(config.videoFileTypes, config.source);
-  moveFiles(videos, config.videoDestination);
+if (config.videoFileTypes && config.videoDestinationFolder) {
+  extractFiles(config.videoFileTypes, config.sourceFolder, config.videoDestinationFolder);
 }
 
 
-function extractFiles(type, source){
-  var files = {};
+function extractFiles(type, source, destination){
+
   for (var i = 0; i < type.length; i++) {
-    files[type[i]] = getFilesIn(source, type[i]);
-    console.log(type[i], files[type[i]].length);
+    console.log("Let's find all the ", type[i], "...")
+
+    var command = 'EXTENSION=' + type[i]
+    + ' MOVE="' + config.moveFiles + '"'
+    + ' SOURCE="' + source + '"'
+    + ' DESTINATION="' + destination + '"'
+    + ' ./extract.sh';
+    shell.exec(command);
   }
-  return files;
+
 }
 
-function getFilesIn(dir, matching){
-  return shell.find(dir).filter(
-    function(file) {
-      return file.toLowerCase().indexOf(matching) > -1;
-    }
-  );
-}
-
-function moveFiles(list, destination) {
-//  console.log(list);
-  console.log(Object.keys(list));
-    Object.keys(list).forEach(function(extension){
-      //console.log(extension, list[extension]);
-    list[extension].map(function(file){
-      if (config.verboseMode) {
-        console.log("copy ", file, 'to ', destination);
-      }
-      if (file && typeof file === "string") {
-        shell.mv(file, destination);
-      } else {
-        console.log(file, " NOT Adestination STRING");
-      }
-    });
-  });
-}
